ffa6f0f10916abb0bc482f319910d1a78826a03c
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-03-05 23:59:35 -0600

    updated: config.js

--- server/config.js
+++ server/config.js
@@ -6,4 +6,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/language-school";
 export const JWT_SECRET = process.env.JWT_SECRET || "secretkey";
 export const ALLOWED_ORIGINS = 
-  ((process.env.ALLOWED_ORIGINS)?.split(",").map(item => item.trim())) || '';
+  ((process.env.ALLOWED_ORIGINS)?.split(",").map(item => item.trim())) || '*';
