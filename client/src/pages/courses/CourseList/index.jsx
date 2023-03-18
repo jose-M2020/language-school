a15d9c28cebae9a15cb5c66d93ce0e8e2772cd1d
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-03-17 23:57:00 -0600

    Courses TabPanel added

--- client/src/pages/courses/CourseList/index.jsx
+++ client/src/pages/courses/CourseList/index.jsx
@@ -1,7 +1,9 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid, Tab, Tabs, Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import { useState } from 'react'
 import Header from '../../../layouts/Dashboard/components/Header'
 import { Course } from './components/Course'
+import TabPanel from './components/TabPanel'
 
 const myCourses = [
   {
@@ -9,7 +11,7 @@ const myCourses = [
     description: 'A new  funny way to encourage kids to learn english',
     price: 1200,
     discount: 120,
-    modality: ['Online', 'In-person'],
+    modality: 'Online',
     levels: [
       {
         name: 'Beginner I',
@@ -60,20 +62,53 @@ const courses = [
   },
 ]
 
+const a11yProps = (index) => {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const CourseList = () => {
-  
+  const [value, setValue] = useState(0);
 
   return (
     <>
       <Header title="COURSES" />
-      <Grid container spacing={2}>
-        <Typography>New courses</Typography>
-        {courses.map((course, i) => (
-          <Grid key={i} item lg={6}>
-            <Course data={course} />
+      <Box mb={8}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs 
+            value={value}
+            onChange={(_, newValue) => setValue(newValue)} 
+            aria-label="basic tabs example"
+          >
+            <Tab label="My Courses" {...a11yProps(0)} />
+            <Tab label="All" {...a11yProps(1)} />
+            <Tab label="Upcoming" {...a11yProps(2)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <Grid container spacing={2}>
+            {myCourses.map((course, i) => (
+              <Grid key={i} item lg={6}>
+                <Course data={course} ispurchased={true} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Grid container spacing={2}>
+            {courses.map((course, i) => (
+              <Grid key={i} item lg={6}>
+                <Course data={course} />
+              </Grid>
+            ))}
+          </Grid>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          No courses
+        </TabPanel>
+      </Box>
     </>
   )
 }
